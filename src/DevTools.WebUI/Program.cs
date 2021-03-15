3b0b5d4c91d2c1cc44f6f163467d4e1e9a16e2bc
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2021-03-15 13:12:36 -0400

    Updated Blazor WASM app to .NET 5

--- src/DevTools.WebUI/Program.cs
+++ src/DevTools.WebUI/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Tewr.Blazor.FileReader;
 
 namespace DevTools.WebUI
 {
@@ -19,7 +18,6 @@ namespace DevTools.WebUI
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddFileReaderService(o => o.UseWasmSharedBuffer = true);
 
             await builder.Build().RunAsync();
         }
