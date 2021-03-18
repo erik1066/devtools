0a073bc9d40de9923b1740e5824d9bd599293b99
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2021-03-18 16:02:44 -0400

    Added Json transformer
    Added a Json transformer capability to the Blazor app based on the JUST.NET NuGet package

---
+++ src/DevTools.WebUI/Models/JsonTransformModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevTools.WebUI.Models
+{
+    public sealed class JsonTransformModel
+    {
+        [Required]
+        [MinLength(1)]
+        public string Input { get; set; } = string.Empty;
+
+        public string Output { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(1)]
+        public string Transform { get; set; } = string.Empty;
+    }
+}
